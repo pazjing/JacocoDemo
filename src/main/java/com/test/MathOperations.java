43a846e9e44239e8aa0d6bb0a3fb2a2bafb60959
Jing Bai <jing.bai@qrious.co.nz>
2017-08-16 12:37:10 +1200

    add more code

--- src/main/java/com/test/MathOperations.java
+++ src/main/java/com/test/MathOperations.java
@@ -4,7 +4,15 @@ public class MathOperations {
 
     public Integer add(Integer a, Integer b)
     {
-        return a+b;
+
+        if (a < 10) {
+
+            return a+b;
+
+        } else {
+
+            return 2*a+b;
+        }
     }
 
     public Integer subtract(Integer a, Integer b)
